a339efe632fd4e8b06ceb68ecb6590c646c2961c
Leandro <lecorvalan2@gmail.com>
2022-12-30 02:19:31 -0300

    Se implementa react-simple-typewriter

---
+++ components/Hero.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Cursor, useTypewriter } from 'react-simple-typewriter';
+
+type Props = {}
+
+export default function Hero({ }: Props) {
+    const [text, count] = useTypewriter({
+        words: [
+            "Hi, my name is Leandro",
+            "Guy-who-loves-memes.tsx",
+            "<ButLovesToCodeMore />"
+        ],
+        loop: true,
+        delaySpeed: 2000,
+    });
+
+    return (
+        <div>
+            <h1>
+                <span>{text}</span>
+                <Cursor cursorColor="#F7AB0A" />
+            </h1>
+        </div>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 export default function Home() {
   return (
-    <>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen">
       <Head>
         <title>Leandro's Portfolio</title>
       </Head>
@@ -12,6 +13,9 @@ export default function Home() {
       <Header />
 
       {/* Hero */}
+      <section id='hero'>
+        <Hero />
+      </section>
 
       {/* About */}
 
@@ -23,6 +27,6 @@ export default function Home() {
 
       {/* Contact Me */}
 
-    </>
+    </ div>
   )
 }
