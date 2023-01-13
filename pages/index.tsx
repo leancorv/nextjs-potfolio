546f29dc643b18989a1d5c24c66e1a95cb681b44
Leandro <lecorvalan2@gmail.com>
2023-01-13 16:45:06 -0300

    Se añade Project section

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import About from '../components/About'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import Projects from '../components/Projects'
 import Skills from '../components/Skills'
 import WorkExperience from '../components/WorkExperience'
 
@@ -35,8 +36,10 @@ export default function Home() {
         <Skills />
       </section>
 
-      {/* Proyects */}
-
+      {/* Projects */}
+      <section id="projects" className='snap-start'>
+        <Projects />
+      </section>
       {/* Contact Me */}
 
     </ div>
