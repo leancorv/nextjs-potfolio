ed29aa5f75620fbb27e870f8914ceb51e164f1e9
Leandro <lecorvalan2@gmail.com>
2023-01-14 16:43:21 -0300

    Se añade Tailwind-scroll-bar

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -17,7 +17,7 @@ function Projects({}: Props) {
                 Projects
             </h3>
 
-            <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20'>
+            <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
                 {projects.map((project, i) => (
                     <div className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
                         <motion.img
@@ -53,7 +53,6 @@ function Projects({}: Props) {
             </div>
 
             <div className='w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12'>
-
             </div>
     </motion.div>
   )
--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -6,12 +6,12 @@ type Props = {}
 
 function WorkExperience({}: Props) {
   return (
-    <motion.div className='h-screen flex relative overflow-hidden flex-col text-lft md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'>
+    <motion.div className='h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'>
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
             Experience
         </h3>
 
-        <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory'>
+        <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
             <ExperienceCard />
             <ExperienceCard />
             <ExperienceCard />
