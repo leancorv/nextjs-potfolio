e6bcb6783cd0b0f1d9bc6d097654e27d850d7774
Leandro <lecorvalan2@gmail.com>
2023-01-14 16:48:50 -0300

    Se añade Home Button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import About from '../components/About'
 import ContactMe from '../components/ContactMe'
 import Header from '../components/Header'
@@ -9,7 +10,7 @@ import WorkExperience from '../components/WorkExperience'
 
 export default function Home() {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
       <Head>
         <title>Leandro's Portfolio</title>
       </Head>
@@ -45,6 +46,19 @@ export default function Home() {
       <section id="contact" className='snap-start'>
         <ContactMe />
       </section>
+
+
+      <Link href="#hero">
+        <footer className='sticky bottom-5 w-full cursor-pointer'>
+          <div className="flex items-center justify-center">
+            <img 
+              className='h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer' 
+              src="https://i.imgur.com/e2yvD6A.png" 
+              alt="" 
+            />
+          </div>
+        </footer>
+      </Link>
 
     </ div>
   )
