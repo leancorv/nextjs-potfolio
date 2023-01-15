42c120d3d753445b6c1b9f5c0a607e1b82216d1d
Leandro <lecorvalan2@gmail.com>
2023-01-15 02:28:13 -0300

    Se empieza a añadir schemas de Sanity

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { SocialIcon } from 'react-social-icons'
 import { motion } from 'framer-motion'
+import Link from 'next/link'
 
 type Props = {}
 
@@ -39,6 +40,8 @@ export default function Header({ }: Props) {
                     bgColor='transparent'
                 />
             </motion.div>
+
+            <Link href='#contact' />
 
             <motion.div
                 initial={{
