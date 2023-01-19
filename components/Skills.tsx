eca3e489af5e5a60ef4f43ad71a2e8196a3564d9
Leandro <lecorvalan2@gmail.com>
2023-01-19 15:41:50 -0300

    Se solucionan bugs de fetch

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -13,7 +13,7 @@ function Skills({ skills }: Props) {
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
         transition={{ duration: 1.5 }}
-        className='flex relative flex-col text-center md:text-left xl:flex-row max-w-[200px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center'
+        className='flex relative flex-col text-center md:text-left xl:flex-row xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center'
     >
             <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
                 Skills
